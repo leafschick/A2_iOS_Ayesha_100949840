85b7179ee29079b284a0839cb0b178fb5e7ccdc6
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-04-01 12:04:38 -0400

    Show first product on home screen

--- A2_iOS_Ayesha_100949840/ContentView.swift
+++ A2_iOS_Ayesha_100949840/ContentView.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+       @FetchRequest(
+           entity: Product.entity(),
+           sortDescriptors: [NSSortDescriptor(key: "productId", ascending: true)]
+       ) private var products: FetchedResults<Product>
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 20) {
@@ -23,8 +31,35 @@ struct ContentView: View {
                 Text("This is an instore app for Starbucks managers to view products and add more products.")
                     .font(.subheadline)
                     .foregroundColor(.black)
-                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                Divider()
+                    .padding(.horizontal)
+                
+                if let firstProduct = products.first {
+                    Text (firstProduct.productName ?? "No Name")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                
                     
+                    Text(firstProduct.productDescription ?? "No Description")
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal)
+                    
+                    Text("Price: $\(firstProduct.productPrice, specifier: "%.2f")")
+                        .font(.headline)
+                    
+                    Text("Provider: \(firstProduct.productProvider ?? "No Provider")")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    
+                } else {
+                    Text("No products available.")
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+                    Spacer()
             }
             .navigationTitle("Home")
         }
@@ -32,5 +67,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+
 }
